926efc48a032beb0ac04da5ae59f282e06b03f4a
MengseuThoeng <mengseu2004@gmail.com>
2025-01-29 03:54:18 +0700

    Fix Eureka

--- src/main/java/com/example/community_engagement/CommunityEngagementApplication.java
+++ src/main/java/com/example/community_engagement/CommunityEngagementApplication.java
@@ -2,8 +2,10 @@ package com.example.community_engagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CommunityEngagementApplication {
 
 	public static void main(String[] args) {
